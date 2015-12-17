5c26a21864c070bda1df4895b588467002cd8a3b
Will Rouesnel <w.rouesnel@gmail.com>
2015-12-18 05:16:09 +1100

    Fix build.

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 	"github.com/wrouesnel/tail_exporter/config"
 	"sync"
 	"os"
+	"strings"
 )
 
 const Namespace string = "tail_collector"
@@ -405,7 +406,7 @@ func main() {
       <body>
       <h1>TCP/UDP Tail Exporter</h1>
       <p>Accepting raw lines over TCP and UDP on ` + *collectorAddress + `</p>
-      <p>Watching files for lines:` + flag.Args() + `</p>
+      <p>Watching files for lines:` + strings.Join(flag.Args(), ", ") + `</p>
       <p><a href="` + *metricsPath + `">Metrics</a></p>
       <h1>Config</h1>
       <pre>` +
